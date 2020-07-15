00f81e421293061036cdd70f0c62fc07d255912d
Carlos <18912144596@163.com>
2020-07-15 11:26:07 +0800

    Update FileUploadService.java

--- src/main/java/com/example/demo/service/FileUploadService.java
+++ src/main/java/com/example/demo/service/FileUploadService.java
@@ -16,8 +16,8 @@ public class FileUploadService {
     public static void uploadPicture(String bucketName, String picName, InputStream stream, long size, String contentType) throws Exception{
         try {
             // 使用MinIO服务的URL，端口，Access key和Secret key创建一个MinioClient对象
-//            MinioClient minioClient = new MinioClient("http://10.21.1.100:9000", "minioadmin", "minioadmin");
-            MinioClient minioClient = new MinioClient("http://120.195.40.209:5035", "minioadmin", "minioadmin");
+//            MinioClient minioClient = new MinioClient("http://xxxxxxx", "xxxxxxx", "xxxxxxx");
+            MinioClient minioClient = new MinioClient("http://xxxxxxx", "xxxxxxx", "xxxxxxx");
 
             // 检查存储桶是否已经存在
             boolean isExist = minioClient.bucketExists(bucketName);
